9641ce2d748a357eb9e7ef01178841fe315c4d99
Lvl3Mage <lvlthreemage@gmail.com>
2022-08-22 22:18:21 +0200

    Removed vue touch events lib

--- src/js/VueSetup.js
+++ src/js/VueSetup.js
@@ -14,10 +14,6 @@ app.use(router);
 import PhosphorVue from "phosphor-vue";
 app.use(PhosphorVue);
 
-import Vue3TouchEvents from "vue3-touch-events";
-
-app.use(Vue3TouchEvents);
-
 app.mount("#app");
 
 export {app};
