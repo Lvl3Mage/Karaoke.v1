9ef8d65232794b143be6589571b36288c5ccc315
Miguelidze <migueldeorpesa@gmail.com>
2022-08-25 01:04:29 +0200

    add modal and swipe

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,3 +1,44 @@
-import Swiper from "swiper";
+import Swiper from 'swiper/bundle';
 
-new Swiper('.swiper-container');
+  // import styles bundle
+import 'swiper/css/bundle';
+  
+new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    grabCursor: true,
+    // Change slide by click on
+    slideToClickedSlides: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+    slidesPerView: 1,
+    watchOverFlow: true,
+    spaceBetween: 25,
+    speed: 850,
+    autoplay: {
+        delay: 2000,
+        stopOnLastSlide: true,
+        disableOnInteraction: true, 
+    },
+    effect: 'cube',
+    cubeEffect: {
+        slideShadows: true,
+        shadow: true,
+        shadowOffset: 20,
+        shadowScale: 1
+    },
+    coverflowEffect: {
+        rotate: 20,
+        stretch: 50,
+        slideShadows: true,
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+});
